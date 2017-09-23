1fec67c20ec9b1516f2a113c216575556be09d3d
Gray-Young <1162032953@qq.com>
2017-09-23 16:27:38 +0800

    ”添加message的model，并且实现添加功能 ;

--- application/home/controller/Index.php
+++ application/home/controller/Index.php
@@ -2,52 +2,44 @@
 namespace app\home\controller;
 
 use think\Controller ;
-use think\Db;
 
 class Index extends Controller
 {
 	// load view
-  public function index()
-  {
-		return $this->fetch('index/index');
-  }
+    public function index()
+    {
+    	return $this->fetch();
+    }
     public function about()
-  {
-		return $this->fetch('index/about');
-  }
+    {
+    	return $this->fetch();
+    }
     public function contact()
-  {
-		return $this->fetch('contact');
-  }
+    {
+    	return $this->fetch();
+    }
     public function join()
-  {
-		return $this->fetch('join');
-  }
-    public function message()
-  {
-		return $this->fetch('message');
-  }
-    public function new_info()
-  {
-		return $this->fetch('new_info');
-  }
+    {
+    	return $this->fetch();
+    }
+    public function info_info()
+    {
+    	return $this->fetch();
+    }
     public function new_list()
-  {
-		return $this->fetch('new_list');
-  }
-    public function product_list()
-  {
-		return $this->fetch('product_list');
-  }
+    {
+    	return $this->fetch();
+    }
     public function product_info()
-  {
-		return $this->fetch('product_info');
-  }
+    {
+    	return $this->fetch();
+    }
+    public function product_list()
+    {
+    	return $this->fetch();
+    }
 
-  public function new_message()
-  {
-      $result = Db::execute('insert into home_message (title, name, phone, email, message) values ("1", "2", "3", "4", "5")');
-      dump($result);
-  }
+
+
 
 }
--- application/home/controller/Message.php
+++ application/home/controller/Message.php
@@ -2,13 +2,29 @@
 	namespace app\home\controller;
 
 	use think\Db;
+	use think\Controller;
+	use think\Request;
+	use app\home\model\Message as MessageModel;
 
-	class Message
+	class Message extends Controller
 	{
-		public function new_message()
+		//渲染在线留言页面
+		public function message()
 		{
-			$result = Db::execute('insert into home_message (title, name, phone, email, message) values ($_GET(), "2", "3", "4", "5")');
-			dump($result);
+			return $this->fetch();
 		}
+		//添加留言
+	    public function add_message()
+	    {
+	        $postData = Request::instance() -> post();
+	        $Message = new MessageModel();
+	        $Message->title = $postData['title'];
+	        $Message->name = $postData['name'];
+	        $Message->phone = $postData['phone'];
+	        $Message->email = $postData['email'];
+	        $Message->message = $postData['message'];
+	        $Message->save();
+	        $this->success("留言成功");
+	    }
 
 	}
