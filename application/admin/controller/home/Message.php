aa55280afa248b10360a92d95d385fbf34bf5717
Gray-Young <1162032953@qq.com>
2017-09-25 00:05:07 +0800

    完善后台对留查看功能,对是否审核更正

--- application/admin/controller/home/Message.php
+++ application/admin/controller/home/Message.php
@@ -6,20 +6,34 @@
 
     class Message extends Controller
     {
-        //列出留言列表
+        /*
+        **列出留言列表
+        **
+        **/
         public function message_list()
         {
-            //实例化一个MessageModel对象
-            $MessageModel = new MessageModel();
-            $messages = $MessageModel->select();
-            //给前台变量赋值
+            //提取全部的数据
+            $messages = MessageModel::all();
+            //给是否审核那一列把数据库里的0变成未审核，1变成已审核
+            foreach($messages as $mess){
+                if ($mess->home_message_audit == 0){
+                    $this->assign('home_message_audit', '未审核');
+                } else {
+                    $this->assign('home_message_audit', '已审核');
+                }
+            }
+            //其他的数据全部原样返回给页面
             $this->assign('messages', $messages);
             //取回打包的数据并且返回给页面
             return $this->fetch();
         }
-        //删除留言
+        /*
+        **删除留言
+        **
+        **/
         public function delete_message($home_message_no)
         {
+            //按照no获取相应的留言
             $MessageModel = MessageModel::get($home_message_no);
             if($MessageModel)
             {
@@ -30,5 +44,21 @@
             {
                 $this->error("删除失败，请核实后重试");
             }
+        }
+        /*
+        **查找相应的留言
+        **
+        **/
+        public function find_message()
+        {
+
+        }
+        /*
+        **查看留言
+        **
+        **/
+        public function detail_message()
+        {
+
         }
     }
