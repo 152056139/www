5db0dee02c4415f3bc455d1b81ace57aaafc705a
Gray-Young <1162032953@qq.com>
2017-09-24 20:50:30 +0800

    添加删除单个留言功能

--- application/admin/controller/home/Message.php
+++ application/admin/controller/home/Message.php
@@ -6,7 +6,8 @@
 
     class Message extends Controller
     {
-        public function messagelist()
+        //列出留言列表
+        public function message_list()
         {
             //实例化一个MessageModel对象
             $MessageModel = new MessageModel();
@@ -15,5 +16,19 @@
             $this->assign('messages', $messages);
             //取回打包的数据并且返回给页面
             return $this->fetch();
+        }
+        //删除留言
+        public function delete_message($home_message_no)
+        {
+            $MessageModel = MessageModel::get($home_message_no);
+            if($MessageModel)
+            {
+                $MessageModel->delete();
+                $this->success("删除成功");
+            }
+            else
+            {
+                $this->error("删除失败，请核实后重试");
+            }
         }
     }
